c86b2afa735731f6635b04ccd4e1e695d1a88fc9
lucemia <lucemia@gmail.com>
2015-06-19 12:23:57 +0800

    init

---
+++ parse.py
@@ -0,0 +1,64 @@
+import requests
+import logging
+import re
+from bs4 import BeautifulSoup
+import csv
+import time
+import jinja2
+
+re_num = re.compile(r"([\d]+)")
+re_url = re.compile(r"rent\-detail\-([\d]+)")
+re_links = re.compile(r'rent\-detail\-[\d]+\.html')
+
+kind = (6, 12) # office, or mix
+
+def parse(kind, offset=0):
+    url = "http://rent.591.com.tw/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=1&orderType=desc&listview=img&kind=%s&firstRow=%s"
+    url = url % (kind, offset*20)
+
+    content = requests.get(url).json()
+
+    # main is for list, recom is recommandation
+    soup = BeautifulSoup(content['main'])
+    for index, div in enumerate(soup.find_all("div", class_="shList")):
+        a = div.find('a', class_='imgbd')
+        title = a['title']
+        link = a['href']
+        address = div.find('div', class_='right')
+        address = "\n".join(k.text for k in address.findChildren()).strip()
+
+        size = div.find('li', class_='area').text.strip()
+        price = div.find('li', class_='price').text.strip()
+
+        img = div.find('img')['src']
+        yield {
+            'title': title,
+            'link': link,
+            'address': address,
+            'size': size,
+            'price': price,
+            'img': img
+        }
+
+    time.sleep(5)
+    if index == 19:
+        for i in parse(kind, offset+1):
+            yield i
+
+def render(filename):
+    with open(filename) as ifile:
+        reader = csv.DictReader(ifile)
+
+        template = jinja2.Template('591.html')
+        template.render({'rows': reader})
+
+def main():
+    with open('results.csv', 'w') as ofile:
+        writer = csv.DictWriter(ofile, ('title', 'link', 'address', 'size', 'price', 'img'))
+        for i in parse(6):
+            writer.writerow([k.encode('utf8') for k in i])
+        for i in parse(12):
+            writer.writerow([k.encode('utf8') for k in i])
+
+if __name__ == '__main__':
+    import clime; clime.start(debug=True)
